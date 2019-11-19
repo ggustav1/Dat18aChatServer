4ca8fbd42eda77019e6ddd0d3a648c8fda7ae8dc
gustavvingtoftandersen <gust2968@stud.kea.dk>
2019-11-19 17:52:04 +0100

    klar til at pulle Client

--- src/chatserver/Server.java
+++ src/chatserver/Server.java
@@ -1,112 +1,32 @@
 package chatserver;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.ArrayList;
 
-public class Server {
-    private ServerSocket serversock;
-    private DataInputStream inputStream;
-    private ArrayList<ChatClient> clients;
+import bg.tusofia.pik3.net.client.Client;
 
-    public Server(int port) {
-        try {
-            this.serversock = new ServerSocket(port);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+/**
+ * An interface describing server's basic functionality.
+ *
+ * @author kaleksandrov
+ *
+ */
+public interface Server {
 
-    }
+    /**
+     * Starts listening for incoming connections on the given port number.
+     */
+    void start();
 
-    /*
+    /**
+     * Disconnects all clients and stop the server.
+     */
+    void stop();
 
-    public ServerSocket getServersock() { return serversock; }
-
-    public void setServersock(ServerSocket serversock) {this.serversock = serversock;}
-
-    public ArrayList<ChatClient> getClients() { return clients; }
-
-    public void setClients(ArrayList<ChatClient> clients) {this.clients = clients;}
-
-    */
-
-    public void run() throws IOException {
-        while(true) {
-
-            Socket socket = null;
-
-            try {
-                // socket object modtager klient anmodninger
-                socket = serversock.accept();
-
-                System.out.println("En ny klient har tilsluttet sig" + socket);
-
-                // modtag input og output stream
-                DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
-                DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
-
-                System.out.println("Tildeler ny tråd til denne klient");
-
-                //Opret ny tråd til objekt
-                Thread thread = new ClientHandler(dataInputStream, dataOutputStream, socket);
-
-                // Invoke start() metoden
-                thread.start();
-
-            } catch (Exception e) {
-                socket.close();
-                e.printStackTrace();
-            }
-        }
-    }
-}
-
-class ClientHandler extends Thread {
-
-    final DataInputStream  dataInputStream;
-    final DataOutputStream dataOutputStream;
-    final Socket socket;
-
-    ClientHandler(DataInputStream dataInputStream, DataOutputStream dataOutputStream, Socket socket) {
-        this.dataInputStream = dataInputStream;
-        this.dataOutputStream = dataOutputStream;
-        this.socket = socket;
-    }
-
-    @Override
-    public void run() {
-        String modtaget;
-        String klarTilRetur;
-
-        // Kør uendeligt loop
-        while (true) {
-            try {
-
-                // Modtag svar fra klient
-                modtaget = dataInputStream.readUTF();
-
-                if (modtaget.equals("QUIT")) {
-                    System.out.println("Klient " + this.socket + "har forladt chatten");
-                    this.socket.close();
-                    System.out.println("Forbindelse lukket");
-                }
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            try {
-                // lukker rescourser
-                this.dataInputStream.close();
-                this.dataOutputStream.close();
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-
-    }
+    /**
+     * Sends a message to all clients connected.
+     *
+     * @param sender
+     *            The client that sends the message.
+     * @param message
+     *            The message to be sent.
+     */
+    void broadcast(final Client sender, final String message);
 }
