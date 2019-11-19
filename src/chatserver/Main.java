389d7ef2f63401dfca21f15db0c774e044fe0529
Barkow <pbarkow@deloitte.dk>
2019-11-19 18:38:32 +0100

    klar til at pulle gustav

--- src/chatserver/Main.java
+++ src/chatserver/Main.java
@@ -1,7 +1,27 @@
 package chatserver;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Main {
     public static void main(String[] args) {
+        Server s = new ServerImpl(2000);
+        s.start();
+    }
 
+    public static void main2(String[] args) {
+        InetAddress ip = null;
+        try {
+            ip = InetAddress.getByName("localhost");
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        try {
+            Socket sock = new Socket(ip, 2000);
+            Client c = new SimpleClientImpl(sock);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
